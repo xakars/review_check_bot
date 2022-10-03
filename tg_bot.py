ee5412a965d1243b6918a3f75e54fe673715d1c1
Ars <xakars02@gmail.com>
2022-10-03 16:30:28 +0600

    prepare for review

---
+++ tg_bot.py
@@ -0,0 +1,29 @@
+import telegram
+import asyncio
+import os
+from checker import check_task_status
+from dotenv import load_dotenv
+
+
+async def send_message(token, chat_id, message):
+    token = token
+    chat_id = chat_id
+    bot = telegram.Bot(token=token)
+    async with bot:
+        #print(await bot.get_me())
+        await bot.send_message(text=message, chat_id=chat_id)
+
+
+def main():
+    load_dotenv()
+    dvmn_token = os.environ['DVMN_TOKEN']
+    tg_token = os.environ["TG_TOKEN"]
+    chat_id = os.environ["TG_CHAT_ID"]
+    while True:
+        message = check_task_status(dvmn_token)
+        asyncio.run(send_message(tg_token, chat_id, message))
+
+
+if __name__ == '__main__':
+    main()
+
