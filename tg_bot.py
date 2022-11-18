ffa0a40c5f01b2a28808fe04f6cb76d2009870d7
Arsen <xakars02@gmail.com>
2022-11-18 18:26:39 +0600

    remove redundant function

--- tg_bot.py
+++ tg_bot.py
@@ -4,18 +4,14 @@ from checker import check_task_status
 from dotenv import load_dotenv
 
 
-def send_message(token, chat_id, message):
-    bot = telegram.Bot(token=token)
-    bot.send_message(text=message, chat_id=chat_id)
-
-
 def main():
     load_dotenv()
     dvmn_token = os.environ['DVMN_TOKEN']
     tg_token = os.environ["TG_TOKEN"]
     chat_id = os.environ["TG_CHAT_ID"]
     message = check_task_status(dvmn_token)
-    send_message(tg_token, chat_id, message)
+    bot = telegram.Bot(token=tg_token)
+    bot.send_message(text=message, chat_id=chat_id)
 
 
 if __name__ == '__main__':
